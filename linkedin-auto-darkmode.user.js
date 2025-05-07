1ea22cd9e5728ba21a2f8597d795753997636606
Naldo DJ <marinaldo.jesus@gmail.com>
2025-05-07 17:31:55 -0300

    Update linkedin-auto-darkmode.user.js
    Fix Login Redirect Loop: Impede a execução do script na página de login para evitar redirecionamentos repetidos ao tentar ativar o modo escuro.

--- linkedin-auto-darkmode.user.js
+++ linkedin-auto-darkmode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LinkedIn - Auto Ativar Modo Escuro (com retorno inteligente)
 // @namespace    https://www.linkedin.com/
-// @version      1.4
+// @version      1.6
 // @description  Ativa o modo escuro automaticamente e retorna à URL original, mesmo ao seguir links externos como do Gmail.
 // @author       Naldo
 // @match        https://www.linkedin.com/*
@@ -15,11 +15,19 @@
     const RETURN_URL_KEY = "linkedinReturnUrl";
 
     const isDarkModePage = window.location.href.includes("/mypreferences/d/dark-mode");
+    const isLoginPage = window.location.href.includes("/uas/login?session_redirect=");
+
+    // Impede a execução do script na página de login
+    if (isLoginPage) {
+        return;
+    }
 
     // Caso 1: Vindo de fora e ainda não ativou o modo escuro
     if (!sessionStorage.getItem(DARKMODE_FLAG) && !isDarkModePage) {
-        // Salva a URL atual completa
-        localStorage.setItem(RETURN_URL_KEY, window.location.href);
+        // Salva a URL atual completa, caso não tenha sido salva
+        if (!localStorage.getItem(RETURN_URL_KEY)) {
+            localStorage.setItem(RETURN_URL_KEY, window.location.href);
+        }
         window.location.href = "https://www.linkedin.com/mypreferences/d/dark-mode";
         return;
     }
